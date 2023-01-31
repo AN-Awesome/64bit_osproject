ce33ef18683ef6552b9f46f5057e6b4b45f8093b
YeontaekPark <parkyt123@naver.com>
2023-01-31 10:51:54 +0900

    feat(Queue.c): add Check Empty State, Insert Function

--- kernel/bits64/src/Queue.c
+++ kernel/bits64/src/Queue.c
@@ -20,3 +20,25 @@ BOOL kIsQueueFull(const QUEUE* pstQueue){
     if((pstQueue->iGetIndex == pstQueue->iPutIndex) && (pstQueue->bLastOperationPut == TRUE)) return TRUE;
     return FALSE;    
 }
+
+// 큐가 비었는지 여부를 반환
+BOOL kIsQueueEmpty(const QUEUE* pstQueue){
+    // 큐의 삽입 인덱스와 제거 인덱스가 같고 마지막으로 수행된 명령이 제거이면
+    // 큐가 비었으므로 제거할 수 없음
+    if((pstQueue->iGetIndex == pstQueue->iPutIndex) && (pstQueue->bLastOperationPut == FALSE)) return TRUE;
+    return FALSE;
+}
+
+// 큐에 데이터를 삽입 
+BOOL kPutQueue(QUEUE* pstQueue, const void* pvData){
+    // 큐가 가득 찼으면 삽입할 수 없음
+    if(kIsQueueFull(pstQueue) == TRUE) return FALSE;
+
+    // 삽입 인덱스가 가리키는 위치에서 데이터의 크기만큼을 복사
+    kMemCpy((char*) pstQueue->pvQueueArray + (pstQueue->iDataSize * pstQueue->iPutIndex), pvData, pstQueue->iDataSize);
+    
+    // 삽입 인덱스를 변경하고 삽입 동작을 수행했음을 기록
+    pstQueue->iPutIndex = (pstQueue->iPutIndex + 1) % pstQueue->iMaxDataCount;
+    pstQueue->bLastOperationPut = TRUE;
+    return TRUE;
+}

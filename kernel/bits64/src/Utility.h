283a25355b33c9c1a437179182d27dae4d82e4da
Awesome <100558608+AN-Awesome@users.noreply.github.com>
2023-01-31 16:02:34 +0900

    feat: add printf(), sprintf(), and Associated functions

--- kernel/bits64/src/Utility.h
+++ kernel/bits64/src/Utility.h
@@ -2,11 +2,16 @@
 #define __UTILITY_H__
 
 #include "Types.h"
+#include <stdarg.h>
 
 void kMemSet(void* pvDestination, BYTE bData, int iSize);
 int kMemCpy(void* pvDestination, const void* pvSource, int iSize);
 int kMemCmp(const void* pvDestination, const void* pvSource, int iSize);
 
 BOOL kSetInterruptFlag(BOOL bEnableInterrupt);
+
+void kCheckTotalRAMSize(void);
+QWORD kGetTotalRAMSize(void);
+void kReverseString(char* pcBuffer);
 
 #endif

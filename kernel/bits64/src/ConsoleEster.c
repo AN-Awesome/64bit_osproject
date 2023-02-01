f570c59a547981a745a15e58a68dcf6fd3bfae5f
YeontaekPark <parkyt123@naver.com>
2023-02-01 15:38:48 +0900

    feat: Ester(PYT)

--- kernel/bits64/src/ConsoleEster.c
+++ kernel/bits64/src/ConsoleEster.c
@@ -1,2 +1,12 @@
 #include "ConsoleEster.h"
 
+{"PYT", "<< Show Our Team Member >>", KShowTeammember}
+
+void KShowTeammember(void) {
+    kSetColor(GREEN);
+    kPrintf(" ===============================\n");
+    kPrintf(" === MASTER : KIM DONG HUI   ===\n");
+    kPrintf(" === SLAVE1 : PARK YEON TAEK ===\n");
+    kPrintf(" === SLAVE2 : JUNG HAE RAM   ===\n");
+    kPrintf(" ===============================\n");
+}
